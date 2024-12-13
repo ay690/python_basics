35c67b2a41770a5740b9c7ea8443ee8ca03a5da3
Aniket Yadav <aniketyadav690@gmail.com>
2024-12-13 17:51:26 +0530

    update: dictionary

---
+++ Chapter/chapter5/dict.py
@@ -0,0 +1,10 @@
+d = {} # Empty dictionary
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23
+}
+
+
+print(marks, type(marks)) # {'Harry': 100, 'Shubham': 56, 'Rohan': 23} <class 'dict'>
+print(marks["Harry"]) # 100
