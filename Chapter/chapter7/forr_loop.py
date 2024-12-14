5da1dd8e60238c485ba6e4569d4d39951787ad4b
Aniket Yadav <aniketyadav690@gmail.com>
2024-12-14 14:02:41 +0530

    update: loops

---
+++ Chapter/chapter7/forr_loop.py
@@ -0,0 +1,12 @@
+for i in range(4):    # this starts from 0
+    print(i)
+
+
+''' 
+Output
+0
+1
+2
+3
+
+'''
---
+++ Chapter/chapter7/list_using_while.py
@@ -0,0 +1,7 @@
+l = [1, "Aniket", False, "This", "Suraj", "Vijay", "Shubhi"]
+
+i = 0
+print(len(l))
+while(i < len(l)):
+    print(l[i])
+    i += 1
