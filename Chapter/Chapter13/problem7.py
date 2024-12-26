bda7dafc1883c2504822706d8efc8cd2023733c3
Aniket Yadav <aniketyadav690@gmail.com>
2024-12-26 19:29:14 +0530

    update: virtual environment creation commands

---
+++ Chapter/Chapter13/problem7.py
@@ -0,0 +1,12 @@
+# pip freeze > requirements.txt
+# virtualenv aniketenv
+
+# To activate vitual env use the command below:
+# .\{name of the env}\Scripts\activate.ps1 
+
+# In this case my env name is aniketenv so the command will be: 
+# .\aniketenv\Scripts\activate.ps1 
+
+# To deactivate the virtual env use the command below:
+# deactivate
+
